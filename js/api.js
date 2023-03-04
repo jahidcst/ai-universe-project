26e056ecbe8131ff0e72d90666b4e6eef561916c
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-03-04 17:12:46 +0600

    api commit

--- js/api.js
+++ js/api.js
@@ -1,11 +1,9 @@
-const allData = () => {
-    const url = ('https://openapi.programming-hero.com/api/ai/tools')
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => showData(data))
-}
-
-const showData = (datas) => {
-    console.log(datas)
-}
-allData();
+// Lets load data form api
+const loadData = () => {
+    const URL = `https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => displayData(data.data.tools));
+  };
+  loadSpinner(true);
+ 
