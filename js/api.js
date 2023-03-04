7ba25d5a413552af3069c9ebd23a167b27ff4230
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-03-04 17:40:20 +0600

    another commit

--- js/api.js
+++ js/api.js
@@ -13,6 +13,36 @@ const loadData = () => {
   
     // For each element from array
     data.forEach((card)) => {
-      // Destructing properties form object
+      // Destructing properties----------//
       const { name, description, image, published_in, features, id } = card;
     }}
+
+
+        // Lets add data into card and append to main container
+  
+        container.innerHTML += `
+        <div class=" w-fit p-5 rounded-lg shadow-2xl">
+        <img class="rounded-lg w-96" src="${image}" alt="">
+        <h1 class="text-xl font-semibold mt-3">Features</h1>
+        <div class="py-3">
+            <ol class="text-base text-slate-500 list-decimal ml-4">
+                <li>${features[0]}</li>
+                <li>${features[1]}</li>
+                <li>${features[2]}</li>
+            </ol>
+        </div>
+        <hr>
+        <div>
+        <h1 class="text-xl font-semibold mt-3">${name}</h1>
+            <div class="flex gap-2 items-center ">
+                <i class="fa-regular fa-calendar-days" ></i>
+                <p>${published_in}</p>
+                <div class="ml-auto"><label onclick="showModal('${id}')" for="my-modal-5"><i class="fa-solid bg-pink-300 p-4 rounded-full fa-arrow-right"></i></label></div>
+                
+            </div>
+        </div>
+    </div>
+        `;
+    loadSpinner(false);
+  });
+};
